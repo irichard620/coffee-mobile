ea84990c4d70afd79824b1e3c189a5bbf695ae85
Ian Richard <ianr620@gmail.com>
2019-08-24 10:37:19 -0700

    Updated tutorial with new assets, added option to replay from settings, fixed bug in brew page

--- src/views/tutorial/tutorial.view.js
+++ src/views/tutorial/tutorial.view.js
@@ -1,13 +1,11 @@
 
 import React, { Component } from 'react';
 import {
-  View, Text, StyleSheet, Image, TouchableOpacity, Dimensions
+  View, Text, StyleSheet, Image, Dimensions
 } from 'react-native';
-
+import Carousel from 'react-native-snap-carousel';
+import Pagination from '../../components/pagination';
 import Button from '../../components/button';
-import Entry from '../home/entry';
-import Add from '../../components/add';
-import * as constants from '../../constants';
 
 class TutorialPage extends Component {
   constructor(props) {
@@ -19,96 +17,121 @@ class TutorialPage extends Component {
 
   onNextClick = () => {
     const { navigation } = this.props;
-    const { step } = this.state;
-
-    if (step < 4) {
-      this.setState({
-        step: step + 1
-      });
+    const fromSettings = navigation.getParam('fromSettings', false);
+    if (fromSettings) {
+      navigation.goBack();
     } else {
       navigation.navigate('Home');
     }
   }
 
-  render() {
-    const { step } = this.state;
+  onSnapToItem = (idx) => {
+    this.setState({ step: idx });
+  }
 
-    const baseButtonPath = '../../assets/buttons/';
+  renderItem = ({ item }) => {
+    const basePath = '../../assets/tutorial/';
 
-    let title = '';
-    if (step === 0) {
-      title = 'Drippy makes great coffee easy. Let’s show you how…';
-    } else if (step === 1) {
-      title = 'View all custom recipes, or just your favorites. When you feel ready, make your own— just tap the plus!';
-    } else if (step === 2) {
-      title = 'Each recipe has its own card.';
-    } else if (step === 3) {
-      title = 'Tap to expand. You can start the recipe… or tap more to edit, unfavorite, or delete it.';
-    } else {
-      title = 'Download cafe recipes, try new beans, and more. Now let’s get to it!';
-    }
-
-    const recipeDescription = 'Inverted with a paper filter\n17g coffee, medium grind\n230g of water, 205°F';
-    let entrySelected = false;
-    if (step === 3) {
-      entrySelected = true;
-    }
-
-    // Entry width
-    const { width } = Dimensions.get('window');
-    const showViewWidth = {
-      width: width * 1
+    const { height } = Dimensions.get('window');
+    const imageHeight = {
+      height: height * 0.42
     };
-    const alignCenter = {
-      alignItems: 'center'
+    const imageContainerHeight = {
+      marginTop: height * 0.148,
+      height: height * 0.42
     };
 
     return (
+      <View style={styles.carouselOutlineInner}>
+        <View>
+          <View style={[styles.iconContainer, imageContainerHeight]}>
+            {item.step === 0 && <Image style={[styles.icon, imageHeight]} source={require(`${basePath}Tutorial_1.png`)} />}
+            {item.step === 1 && <Image style={[styles.icon, imageHeight]} source={require(`${basePath}Tutorial_2.png`)} />}
+            {item.step === 2 && <Image style={[styles.icon, imageHeight]} source={require(`${basePath}Tutorial_3.png`)} />}
+            {item.step === 3 && <Image style={[styles.icon, imageHeight]} source={require(`${basePath}Tutorial_4.png`)} />}
+          </View>
+          <View style={styles.textview}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.description}>{item.description}</Text>
+          </View>
+        </View>
+        {item.button && (
+        <View style={styles.buttonContainer}>
+          <Button
+            onButtonClick={this.onNextClick}
+            type={1}
+            title="Let's Go!"
+            width={110}
+            margin={[0, 0, 0, 0]}
+          />
+        </View>
+        )}
+      </View>
+    );
+  }
+
+  render() {
+    const { step } = this.state;
+
+    // Get data
+    const data = [
+      {
+        title: 'Welcome to Drippy',
+        description: "Ready to make some great coffee? It's easy -- here's all you need to know.",
+        step: 0,
+        button: false
+      },
+      {
+        title: 'Coffee Spotlight',
+        description: 'Drippy helps you discover the hottest roasters and cafes around. '
+          + "See where they're located, learn about their beans, and try their recipes yourself. "
+          + 'Brew like the best!',
+        step: 1,
+        button: false
+      },
+      {
+        title: 'Recipe Cards',
+        description: 'Each recipe has its own card on your dashboard. Tap to expand, see more details '
+          + 'and start the step-by-step brew guide.',
+        step: 2,
+        button: false
+      },
+      {
+        title: 'Brew and Discover',
+        description: "There's so much good coffee out there waiting for you -- and it's not going to brew itself.",
+        step: 3,
+        button: true
+      }
+    ];
+
+    // Width, height
+    const { width, height } = Dimensions.get('window');
+    const carouselHeight = {
+      height: height * 0.85
+    };
+
+    // switch to using carousel with new images
+    return (
       <View style={styles.container}>
-        {step === 1
-            && (
-              <View style={[styles.showview, showViewWidth, alignCenter]}>
-                <Button
-                  type={1}
-                  title="Favorites"
-                  width="39%"
-                  margin={[0, 0, 10, 0]}
-                  disabled
-                />
-                <Button
-                  type={1}
-                  title="Custom"
-                  width="39%"
-                  margin={[0, 0, 10, 0]}
-                  disabled
-                />
-                <Add
-                  type={1}
-                  disabled
-                />
-              </View>
-            )}
-        {(step === 2 || step === 3)
-            && (
-              <View style={[styles.showview, showViewWidth]}>
-                <Entry
-                  disabled
-                  idx={0}
-                  selected={entrySelected}
-                  vessel={constants.VESSEL_AEROPRESS}
-                  title="Intro to the Aeropress"
-                  description={recipeDescription}
-                />
-              </View>
-            )}
-        <View style={styles.textview}>
-          <Text style={styles.title}>{title}</Text>
+        <View style={[styles.carouselOutline, carouselHeight]}>
+          <Carousel
+            data={data}
+            renderItem={this.renderItem}
+            sliderWidth={width}
+            itemWidth={width}
+            hasParallaxImages={false}
+            inactiveSlideScale={1}
+            inactiveSlideOpacity={1}
+            loop={false}
+            autoplay={false}
+            onSnapToItem={snapIdx => this.onSnapToItem(snapIdx)}
+          />
         </View>
-        <View style={styles.buttonview}>
-          <TouchableOpacity onPress={this.onNextClick}>
-            <Image style={[styles.mini]} source={require(`${baseButtonPath}Go.png`)} />
-          </TouchableOpacity>
-        </View>
+        <Pagination
+          total={4}
+          index={step}
+          activeColor="#1D5E9E"
+        />
       </View>
     );
   }
@@ -118,21 +141,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F4F4F4',
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: 'flex-start'
   },
   showview: {
     position: 'absolute',
     top: '10%',
   },
   textview: {
-    width: '90%',
-    position: 'absolute',
-    top: '41%',
+    alignItems: 'center',
+    paddingLeft: 15,
+    paddingRight: 15
   },
   title: {
-    fontSize: 28,
+    fontSize: 22,
     fontWeight: '600',
     color: '#1D5E9E',
+    textAlign: 'center'
+  },
+  description: {
+    marginTop: 10,
+    fontSize: 16,
+    color: '#333333',
     textAlign: 'center'
   },
   buttonview: {
@@ -140,9 +170,20 @@ const styles = StyleSheet.create({
     top: '80%',
     width: 40,
   },
-  mini: {
-    height: 40,
-    width: 40
+  carouselOutline: {
+    width: '100%'
+  },
+  carouselOutlineInner: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    marginBottom: 40
+  },
+  icon: {
+    resizeMode: 'contain',
+    width: '100%'
   }
 });
 
